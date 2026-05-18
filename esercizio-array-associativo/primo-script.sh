b4fd65e6928d5ee9570d375c0372e010d80cf93d
NAMANARI36 <99763224+NAMANARI36@users.noreply.github.com>
2026-05-18 12:35:26 +0200

    Create primo-script.sh

---
+++ esercizio-array-associativo/primo-script.sh
@@ -0,0 +1,1 @@
+echo "Geppetto:Gabriele:VAlerio:GiuLIA:FABIANO:MeRiSiTa:Annie:JnnNY:ANITA:MeRiSiTa:Anniei:Gabriele:VAlerio:GiuLIA:BRUNO:ROBERTO" | tr ':' '\n' | tr '[:upper:]' '[:lower:]' | sort | uniq
