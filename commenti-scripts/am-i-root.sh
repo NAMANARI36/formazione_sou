9735a2b5ebed5ce7fb87fe058495b0cff3117a41
NAMANARI36 <99763224+NAMANARI36@users.noreply.github.com>
2026-05-18 17:06:58 +0200

    Update am-i-root.sh

--- commenti-scripts/am-i-root.sh
+++ commenti-scripts/am-i-root.sh
@@ -3,14 +3,14 @@
 
 ROOT_UID=0   # Root has $UID 0.
 
-if [ "$UID" -eq "$ROOT_UID" ]  # Will the real "root" please stand up?
+if [ "$UID" -eq "$ROOT_UID" ]  # ID Comparison to check if the user is root
 then
-  echo "You are root."
+  echo "You are root."         # Prints
 else
-  echo "You are just an ordinary user (but mom loves you just the same)."
+  echo "You are just an ordinary user (but mom loves you just the same)."   # Prints
 fi
 
-exit 0
+exit 0 # Positive exit code
 
 # ============================================================= #
 # Code below will not execute, because the script already exited.
@@ -19,10 +19,10 @@ exit 0
 
 ROOTUSER_NAME=root
 
-username=`id -nu`              # Or...   username=`whoami`
-if [ "$username" = "$ROOTUSER_NAME" ]
+username=`id -nu`                         # Prints the user username
+if [ "$username" = "$ROOTUSER_NAME" ]     # Comparison to check if the user username is root
 then
-  echo "Rooty, toot, toot. You are root."
+  echo "Rooty, toot, toot. You are root."  # Prints
 else
-  echo "You are just a regular fella."
+  echo "You are just a regular fella."     # Prints
 fi
