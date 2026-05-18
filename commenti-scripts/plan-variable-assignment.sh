35b7211afb277e84bfa2a2b4e3e606e5991292eb
NAMANARI36 <99763224+NAMANARI36@users.noreply.github.com>
2026-05-18 17:20:50 +0200

    Update plan-variable-assignment.sh

--- commenti-scripts/plan-variable-assignment.sh
+++ commenti-scripts/plan-variable-assignment.sh
@@ -7,18 +7,18 @@ echo
 # When it is being assigned, rather than referenced.
 
 # Assignment
-a=879
-echo "The value of \"a\" is $a."
+a=879   # Declaration and inizialization of a variable
+echo "The value of \"a\" is $a."  # Prints
 
 # Assignment using 'let'
-let a=16+5
-echo "The value of \"a\" is now $a."
+let a=16+5 # Sum of two numbers
+echo "The value of \"a\" is now $a."  # Prints
 
 echo
 
 # In a 'for' loop (really, a type of disguised assignment):
 echo -n "Values of \"a\" in the loop are: "
-for a in 7 8 9 11
+for a in 7 8 9 11   # For loop
 do
   echo -n "$a "
 done
@@ -33,4 +33,4 @@ echo "The value of \"a\" is now $a."
 
 echo
 
-exit 0 
+exit 0 # Positive exit code
